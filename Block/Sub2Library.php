eb85430964e0a6efcbf3531841d51cbf090b5037
Adam Moss <adammoss7@gmail.com>
2017-04-06 15:50:23 +0100

    Adds Sub2 Library to every page

---
+++ Block/Sub2Library.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Space48\SubTech\Block;
+
+use Magento\Framework\View\Element\Template;
+
+class Sub2Library extends Template {
+
+    /**
+     * SubTech Helper
+     *
+     * @var \Space48\SubTech\Helper\Data
+     */
+    protected $sub2Helper = null;
+
+    /**
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Framework\Json\Helper\Data $jsonHelper
+     * @param GtmHelper $gtmHelper
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Space48\SubTech\Helper\Data $sub2Helper,
+        array $data = []
+    ) {
+        $this->sub2Helper = $sub2Helper;
+
+        parent::__construct(
+            $context,
+            $data
+        );
+    }
+
+    public function isEnabled()
+    {
+        return $this->sub2Helper->isEnabled()
+            ? true : false;
+    }
+
+    public function getLicenceKey()
+    {
+        return $this->sub2Helper->getConfig('licence_key');
+    }
+}
---
+++ Helper/Data.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Space48\SubTech\Helper;
+
+class Data extends \Magento\Framework\App\Helper\AbstractHelper {
+
+    const XML_PATH = 's48_sub2tech/settings/';
+
+    /**
+     * @var \Magento\Framework\ObjectManagerInterface
+     */
+    protected $_objectManager;
+
+    /**
+     * @param \Magento\Framework\App\Helper\Context $context
+     * @param \Magento\Framework\ObjectManagerInterface
+     */
+    public function __construct(
+        \Magento\Framework\App\Helper\Context $context, \Magento\Framework\ObjectManagerInterface $objectManager
+    ) {
+        $this->_objectManager = $objectManager;
+        parent::__construct($context);
+    }
+
+    public function isEnabled() {
+
+        if ($this->scopeConfig->isSetFlag(self::XML_PATH."active", \Magento\Store\Model\ScopeInterface::SCOPE_STORE)
+        && $this->getConfig('licence_key') != "") {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getConfig($field) {
+        return $this->scopeConfig->getValue(self::XML_PATH.$field, \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+    }
+
+}
